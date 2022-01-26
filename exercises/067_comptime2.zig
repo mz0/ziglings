1501fc61f854b644186d0796ce8df3a5f1d0888b
Mark Zhitomirski <mz@exactpro.com>
2022-01-26 20:12:29 +0300

    done 64-68

--- exercises/067_comptime2.zig
+++ exercises/067_comptime2.zig
@@ -35,15 +35,18 @@ pub fn main() void {
     // In this contrived example, we've decided to allocate some
     // arrays using a variable count! But something's missing...
     //
-    var count = 0;
+    comptime var count = 0;
 
     count += 1;
+    // @compileLog("Count at compile time: ", count);
     var a1: [count]u8 = .{'A'} ** count;
 
     count += 1;
+    // @compileLog("Count at compile time: ", count);
     var a2: [count]u8 = .{'B'} ** count;
 
     count += 1;
+    // @compileLog("Count at compile time: ", count);
     var a3: [count]u8 = .{'C'} ** count;
 
     count += 1;
@@ -60,5 +63,5 @@ pub fn main() void {
     //
     // Try uncommenting this line and playing around with it
     // (copy it, move it) to see what it does:
-    //@compileLog("Count at compile time: ", count);
+    // @compileLog("Count at compile time: ", count);
 }
